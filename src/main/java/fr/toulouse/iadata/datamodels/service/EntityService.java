d63c5f816a2c2f74e5cef55638462ff103b5eb3f
AlexandreClose <alexandre.close@gmail.com>
2020-04-21 13:03:58 +0200

    Go on with processor refactoring

--- src/main/java/fr/toulouse/iadata/datamodels/service/EntityService.java
+++ src/main/java/fr/toulouse/iadata/datamodels/service/EntityService.java
@@ -2,13 +2,63 @@ package fr.toulouse.iadata.datamodels.service;
 
 import fr.toulouse.iadata.datamodels.models.ngsi.AbstractProperty;
 import fr.toulouse.iadata.datamodels.models.ngsi.Entity;
+import fr.toulouse.iadata.datamodels.models.ngsi.EntityMember;
+import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
+@Component
 public class EntityService
 {
+    public EntityMember getEntityMemberByPath( Entity entity, String strPath )
+    {
+        String[] keyPath = strPath.split("[.]");
+        if ((keyPath.length == 1))
+        {
+            return entity.getMembers().get( keyPath[0]);
+        }
+        else
+        {
+            EntityMember member = entity.getMembers().get( keyPath[0] );
+            for (int i=1;i< keyPath.length; i++){
+                if ( member.getMembers().containsKey( keyPath[i])){
+
+                    member= member.getMembers().get( keyPath[i] );
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            return member;
+        }
+
+    }
+
+    public void replaceEntityMemberName( Entity entity, String oldKey, String newKey )
+    {
+        String[] oldKeyPath = oldKey.split("[.]");
+        String[] newKeyPath= newKey.split("[.]");
+        if ((oldKeyPath.length == 1) || (!oldKeyPath[0].equals(newKeyPath[0]))){
+            entity.getMembers().get( oldKeyPath[0]).setName(newKeyPath[0] );
+            entity.getMembers().put( newKeyPath[0],entity.getMembers( ).get( oldKeyPath[0] ));
+            entity.getMembers().remove(oldKeyPath[0]);
+            oldKeyPath[0]=newKeyPath[0];
+        }
+            EntityMember member = entity.getMembers().get( oldKeyPath[0] );
+            for (int i=1;i< oldKeyPath.length; i++){
+                if ( member.getMembers().containsKey( oldKeyPath[i]) && !oldKeyPath[i].equals(newKeyPath[i])){
+
+                    member.getMembers().get(oldKeyPath[i]).setName(newKeyPath[i]);
+                    member.getMembers().put(newKeyPath[i],member.getMembers( ).get( oldKeyPath[i] ));
+                    member.getMembers().remove(oldKeyPath[i]);
+                }
+                member=member.getMembers().get(newKeyPath[i]);
+            }
+    }
+
     public void consumeOnNamedProperties(Entity entity, String strPropertyName, Consumer<AbstractProperty> consumer )
     {
         Stream<AbstractProperty> streamProperties = flatten( entity );
