32e3ad3438a621f07455336cb056f676e5ccdf23
AlexandreClose <alexandre.close@gmail.com>
2020-04-28 10:37:14 +0200

    Merge branch 'develop' of C:\Users\alexa\IdeaProjects\iadata-lib-datamodels with conflicts.

---
+++ src/main/java/fr/toulouse/iadata/datamodels/service/Application.java
@@ -0,0 +1,46 @@
+package fr.toulouse.iadata.kafkaprocessors.service;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Accessors( prefix = {"_str","_"})
+@Document( indexName = "kafka-streams-applications")
+public class Application
+{
+    @Id
+    private String _id;
+    @Field( name = "topicIn")
+    private String _strTopicIn;
+    @Field( name = "topicOut")
+    private String _strTopicOut;
+    @Field( name = "name")
+    private String _strName;
+    @Field( name = "direction")
+    private String _strDirection;
+    @Field( name = "serdeClassName")
+    private String _serdeClassName;
+    @Field( name = "mapKeyNames")
+    private Map<String,String> _mapKeyNames = new HashMap<>();
+    @Field( name = "oldKeyNames")
+    private List<String> _oldKeyNames = new ArrayList<>();
+    @Field( name = "newKeyNames")
+    private List<String> _newKeyNames = new ArrayList<>();;
+    @Field( name="processors", type= FieldType.Nested )
+    private List<Processor> _processors = new ArrayList<>();
+
+    public void addProcessor( Processor processor)
+    {
+        _processors.add( processor );
+    }
+
+}
