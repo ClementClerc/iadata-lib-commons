bf3b73c15638d382e869a14951ff851d8c170243
LOUIT Gérald <cu33443@mairie.toulouse.intra>
2020-10-12 14:52:08 +0200

    modification of RelationShip class to accept uri arrays

--- src/main/java/fr/toulouse/iadata/datamodels/models/ngsi/Relationship.java
+++ src/main/java/fr/toulouse/iadata/datamodels/models/ngsi/Relationship.java
@@ -7,10 +7,17 @@ package fr.toulouse.iadata.datamodels.models.ngsi;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import lombok.*;
-import lombok.experimental.Accessors;
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j2;
 
 /**
  * This class represents NGSI Relationship described here
@@ -19,12 +26,14 @@ import lombok.experimental.Accessors;
 @Data
 @EqualsAndHashCode(callSuper=true)
 @NoArgsConstructor
+@Log4j2
 public class Relationship extends EntityMember
 {
     private static final String TYPE = "Relationship";
 
+
     @Builder
-    public Relationship( AbstractProperty createdAt, AbstractProperty modifiedAt, String name, URI datasetId, String observedAt, Map<String, EntityMember> members, String object) throws URISyntaxException
+    public Relationship( AbstractProperty createdAt, AbstractProperty modifiedAt, String name, URI datasetId, String observedAt, Map<String, EntityMember> members, Object object) throws URISyntaxException
     {
         this.createdAt = createdAt;
         this.modifiedAt = modifiedAt;
@@ -33,10 +42,21 @@ public class Relationship extends EntityMember
         this.observedAt = observedAt;
         this.type = TYPE;
         this.members = members;
-        this.object = new URI(object);
+        if (object instanceof ArrayList){
+            try {
+                this.object = (List<URI>) object;
+            } catch (ClassCastException e) {
+                log.error("ClassCastException expected URI List "+ e.getMessage());
+            }
+        }
+        else if( object != null ) {
+        this.object = new URI(object.toString());
+        }
     }
 
-    private URI object;
+    private Object object;
+
+
     
     @Override
     public void setType() {
@@ -48,4 +68,25 @@ public class Relationship extends EntityMember
     {
         return super.clone();
     }
+
+
+    public static class RelationshipBuilder
+    {
+
+        public RelationshipBuilder addListURI( String strUri) throws URISyntaxException
+        {
+            if ( object == null )
+            {
+                object = new ArrayList<>();
+            }
+            try {
+                ((List<URI> )object).add( new URI(strUri) );
+            } catch (ClassCastException e) {
+                log.error("ClassCastException expected URI List " + e.getMessage());
+            }
+            return this;
+        }
+    }
+
+
 }
