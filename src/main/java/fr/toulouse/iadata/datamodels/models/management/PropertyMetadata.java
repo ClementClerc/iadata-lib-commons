a6f0f5a678f9a3fd6b2f88c8597ff46d4850e8e2
GegeLou <cu33443@mairie.toulouse.intra>
2020-12-22 11:23:19 +0100

    Revert "modify propertyMetadata for _links"
    This reverts commit 38ad39a1628b959cc090e179338a63adabd9f8d1.

--- src/main/java/fr/toulouse/iadata/datamodels/models/management/PropertyMetadata.java
+++ src/main/java/fr/toulouse/iadata/datamodels/models/management/PropertyMetadata.java
@@ -1,17 +1,8 @@
 package fr.toulouse.iadata.datamodels.models.management;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import fr.toulouse.iadata.datamodels.models.ngsi.Context;
-import fr.toulouse.iadata.datamodels.models.serde.ContextDeserializer;
-import fr.toulouse.iadata.datamodels.models.serde.ContextSerializer;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,8 +19,6 @@ public class PropertyMetadata
     private List<String> categories;
     private PropertyDataType propertyDataType;
     private List<String> linkedPipelines = new ArrayList<>();
-    @JsonIgnore
-    private List<Object> _links = new ArrayList<Object>();
 
     public void addLinkedPipelines( String pipelineName )
     {
